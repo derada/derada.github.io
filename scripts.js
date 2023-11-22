9e4408d5b83511a2121e6bd312aa5d3cf3d03408
derada <60904442+derada@users.noreply.github.com>
2023-11-21 21:20:04 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var navbarItems = `<ul>
+                     <li>Home</li>
+                     <li>About</li>
+                     <li>Contact</li> 
+                   </ul>
+`
+document.body.insertAdjacentHTML("afterbegin", navbarItems);
