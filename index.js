65b5a0121e49aad4fadb057ac75968e8cd1d05cf
derada <60904442+derada@users.noreply.github.com>
2023-11-21 23:53:39 -0600

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-// Get the template element
-let template = document.getElementById("header");
-// Clone the template content
-let clone = template.content.cloneNode(true);
-// Insert the clone into the document body
-// document.body.appendChild(clone);
-document.body.insertAdjacentHTML(clone);
